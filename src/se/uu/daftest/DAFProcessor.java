58a76f3308cfa7e79564a13c80df0208c3aeec0c
Charitha Elvitigala <charitha@scorelab.org>
2015-05-16 12:54:18 +0530

    stutter existing implementation added to the repo

---
+++ src/se/uu/daftest/DAFProcessor.java
@@ -0,0 +1,291 @@
+package se.uu.daftest;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder.AudioSource;
+import android.util.Log;
+
+public class DAFProcessor implements Runnable {
+
+    private static final int DEFAULT_RW_SIZE = 256;
+
+    private static final int DEFAULT_SAMPLE_RATE = 48000;
+
+    private static final int MAX_DEALY = 1000;
+
+    private static final int MIN_DEALY = 0;
+
+    /**
+     * This is set to true if DAF processing is active
+     */
+    private boolean isActive = false;
+
+    /**
+     * Object for processing thread
+     */
+    private Thread processingThread = null;
+
+    /**
+     * This object is used to lock bufReadOffset and bufWriteOffset
+     */
+    private Object lockObject = null;
+
+    /**
+     * This is set to true if DAFProcessor is initialized
+     */
+    private boolean isInitialized = false;
+
+    /**
+     * An object for recording audio
+     */
+    AudioRecord recorder = null;
+
+    /**
+     * An object for playing audio data
+     */
+    AudioTrack track = null;
+
+    /**
+     * The buffer for audio samples
+     */
+    short[] dataBuffer = null;
+
+    /**
+     * The delay to add
+     */
+    int delay = MAX_DEALY;
+
+    /**
+     * Sample rate for recording and playing audio
+     */
+    int sampleRate = 48000;
+
+    /**
+     * Minimum size of the buffer that is required before overflowing data.
+     */
+    int recordMinBufSize = 256;
+
+    /**
+     * Minimum size of the buffer that is required for AudioTrack before running
+     * out of data.
+     */
+    int playMinBufSize = 512;
+
+    /**
+     * The offset of the buffer for writing
+     */
+    int bufWriteOffset = 0;
+
+    /**
+     * The offset of the buffer for reading.
+     */
+    int bufReadOffset = 0;
+
+    /**
+     * The size of data to be read or written from/to buffer at a time.
+     */
+    int bufRWSize = 512;
+
+    public DAFProcessor() {
+        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+        isInitialized = false;
+        delay = MIN_DEALY;
+        sampleRate = DEFAULT_SAMPLE_RATE;
+        recordMinBufSize = DEFAULT_RW_SIZE;
+        playMinBufSize = DEFAULT_RW_SIZE;
+        bufRWSize = DEFAULT_RW_SIZE;
+        lockObject = new Object();
+    }
+
+    /**
+     * Start processing
+     */
+    public void StartProcessing() {
+        if (isActive == true) {
+            Log.i(this.getClass().getName(), "Already active");
+            return;
+        } else {
+            processingThread = new Thread(this);
+            processingThread.start();
+            isActive = true;
+        }
+    }
+
+    /**
+     * Stop processing
+     */
+    public void StopProcessing() {
+        if (isActive == false) {
+            Log.i(this.getClass().getName(), "Already stopped");
+            return;
+        } else {
+            isActive = false;
+            try {
+                processingThread.join();
+            } catch (InterruptedException e) {
+                Log.w(this.getClass().getName(), e);
+            }
+            processingThread = null;
+        }
+
+    }
+
+    /**
+     * Return whether the processor is processing
+     * 
+     * @return true of if processing
+     */
+    public boolean IsProcessing() {
+        return isActive;
+    }
+
+    /**
+     * Initialize DAFProcessor
+     */
+    public void Initialize() {
+        Log.i(this.getClass().getName(), "Initializing Audio devices");
+
+        
+        Log.i(this.getClass().getName(), "getNativeOutputSampleRate() : " + 
+                                          AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_SYSTEM));
+        
+        recordMinBufSize = AudioRecord.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO,
+                                                        AudioFormat.ENCODING_PCM_16BIT);
+        Log.i(this.getClass().getName(), "AudioRecord Min Buffer Size " + recordMinBufSize);
+        playMinBufSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                                                     AudioFormat.ENCODING_PCM_16BIT);
+        Log.i(this.getClass().getName(), "AudioTrack Min Buffer Size " + playMinBufSize);
+
+        // We chose the largest buffer size for smooth operation
+        bufRWSize = playMinBufSize > recordMinBufSize ? recordMinBufSize : playMinBufSize;
+
+        int maxDelaySamples = (sampleRate / 1000) * MAX_DEALY;
+        dataBuffer = new short[maxDelaySamples + bufRWSize];
+    }
+
+    @Override
+    public void run() {
+        Log.i(this.getClass().getName(), "Running Audio Thread");
+
+        if (isInitialized == false) {
+            Initialize();
+            SetDelay(0);
+            isInitialized = true;
+        }
+
+        try {
+
+            recorder = new AudioRecord(AudioSource.DEFAULT, sampleRate, AudioFormat.CHANNEL_IN_MONO,
+                                       AudioFormat.ENCODING_PCM_16BIT, bufRWSize * 10);
+
+            track = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                                   AudioFormat.ENCODING_PCM_16BIT, playMinBufSize,
+                                   AudioTrack.MODE_STREAM);
+
+            recorder.startRecording();
+            track.play();
+
+            int N = 0;
+            int writeOffset = 0;
+            int readOffset = 0;
+            while (isActive == true) {
+
+                synchronized (lockObject) {
+
+                    if (dataBuffer.length < bufWriteOffset + bufRWSize) {
+                        if (bufWriteOffset > bufReadOffset) {
+                            java.lang.System.arraycopy(dataBuffer, bufReadOffset, dataBuffer, 0,
+                                                       bufWriteOffset - bufReadOffset);
+                            bufWriteOffset = bufWriteOffset - bufReadOffset;
+                            bufReadOffset = 0;
+                        } else {
+                            // / This situation cannot be occurred.
+                            bufWriteOffset = 0;
+                            bufReadOffset = 0;
+                        }
+                    }
+
+                    writeOffset = bufWriteOffset;
+                    readOffset = bufReadOffset;
+                }
+
+                N = recorder.read(dataBuffer, writeOffset, bufRWSize);
+                if (N != AudioRecord.ERROR_INVALID_OPERATION || N != AudioRecord.ERROR_BAD_VALUE) {
+                    track.write(dataBuffer, readOffset, bufRWSize);
+                    synchronized (lockObject) {
+                        bufWriteOffset += bufRWSize;
+                        bufReadOffset += bufRWSize;
+                    }
+                }
+            }
+
+        } catch (Throwable x) {
+            Log.w(this.getClass().getName(), "Error reading voice audio", x);
+        }
+
+        finally {
+            recorder.stop();
+            recorder.release();
+            track.stop();
+            track.release();
+        }
+
+    }
+
+    /**
+     * Set delay
+     * 
+     * @param millisec
+     *            The delay in milliseconds
+     */
+    public void SetDelay(int millisec) {
+        
+        if (isInitialized == true) {
+            Initialize();
+        }
+        
+        synchronized (lockObject) {
+            delay = millisec;
+
+            if (millisec == 0) {
+                bufReadOffset = 0;
+                bufWriteOffset = 0;
+            } else {
+                int delaySamples = (sampleRate / 1000) * delay;
+                java.util.Arrays.fill(dataBuffer, 0, delaySamples - 1, (short) 0);
+                bufReadOffset = 0;
+                bufWriteOffset = delaySamples;
+            }
+        }
+    }
+
+    /**
+     * Returns the current delay
+     * 
+     * @return Delay
+     */
+    public int GetDelay() {
+        return delay;
+    }
+
+    /**
+     * Returns the maximum delay
+     * 
+     * @return The maximum delay
+     */
+    public static int GetMaxDealy() {
+        return MAX_DEALY;
+    }
+
+    /**
+     * Returns the minimum delay
+     * 
+     * @return The minimum delay
+     */
+    public static int GetMinDealy() {
+        return MIN_DEALY;
+    }
+
+}
---
+++ src/se/uu/daftest/MainActivity.java
@@ -0,0 +1,125 @@
+package se.uu.daftest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener, OnSeekBarChangeListener {
+
+    static Map<String, Object> stateMap = new HashMap<String, Object>();
+
+    DAFProcessor audio = null;
+    Button buttonStart = null;
+    Button buttonStop = null;
+    TextView textStatus = null;
+    SeekBar seekBarDelay = null;
+    TextView textDelay = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (stateMap.containsKey("audio") == false) {
+            audio = new DAFProcessor();
+            stateMap.put("audio", audio);
+        } else {
+            audio = (DAFProcessor) stateMap.get("audio");
+        }
+
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+        textStatus = (TextView) findViewById(R.id.textViewStatus);
+
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+
+        if (audio.IsProcessing() == true) {
+            buttonStart.setEnabled(false);
+            buttonStop.setEnabled(true);
+            textStatus.setText("Started");
+        } else {
+            buttonStart.setEnabled(true);
+            buttonStop.setEnabled(false);
+            textStatus.setText("Stopped");
+        }
+
+        seekBarDelay = (SeekBar) findViewById(R.id.seekBarDelay);
+        seekBarDelay.setOnSeekBarChangeListener(this);
+        int progress = ((audio.GetDelay() - DAFProcessor.GetMinDealy()) * 100)
+                        / (DAFProcessor.GetMaxDealy() - DAFProcessor.GetMinDealy());
+        Log.i("Progress", "Progress : " + progress);
+        seekBarDelay.setProgress(progress);
+
+        textDelay = (TextView) findViewById(R.id.textViewDelay);
+        textDelay.setText(Integer.toString(audio.GetDelay()));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle stateToBeSaved) {
+        if (audio != null) {
+            stateMap.put("audio", audio);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int ID = view.getId();
+        if (ID == buttonStart.getId()) {
+            audio.StopProcessing();
+            buttonStart.setEnabled(false);
+            audio.StartProcessing();
+            buttonStop.setEnabled(true);
+            textStatus.setText("Started");
+
+        } else if (ID == buttonStop.getId()) {
+            audio.StopProcessing();
+            buttonStop.setEnabled(false);
+            buttonStart.setEnabled(true);
+            textStatus.setText("Stopped");
+        }
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar v, int progress, boolean isUser) {
+        if (textDelay != null) {
+            int delay = (progress * (DAFProcessor.GetMaxDealy() - DAFProcessor.GetMinDealy()) / 100)
+                            + DAFProcessor.GetMinDealy();
+            textDelay.setText(Integer.toString(delay));
+        }
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        int delay = (seekBar.getProgress()
+                        * (DAFProcessor.GetMaxDealy() - DAFProcessor.GetMinDealy()) / 100)
+                        + DAFProcessor.GetMinDealy();
+        audio.SetDelay(delay);
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+}

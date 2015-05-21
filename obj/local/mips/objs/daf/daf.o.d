4b0cffc3eea9e5c6d2e8c771fa513b6a1641a151
rhperera <harith9127@gmail.com>
2015-05-21 21:32:43 +0530

    new Activity added with NDK support.

---
+++ obj/local/mips/objs/daf/daf.o.d
@@ -0,0 +1,19 @@
+obj/local/mips/objs/daf/daf.o: jni/daf.c \
+ E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/string.h \
+ E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/sys/cdefs.h \
+ E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/sys/cdefs_elf.h \
+ E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/android/api-level.h \
+ E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/malloc.h \
+ E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/jni.h
+
+E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/string.h:
+
+E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/sys/cdefs.h:
+
+E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/sys/cdefs_elf.h:
+
+E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/android/api-level.h:
+
+E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/malloc.h:
+
+E:/Android/android-ndk/platforms/android-19/arch-mips/usr/include/jni.h:

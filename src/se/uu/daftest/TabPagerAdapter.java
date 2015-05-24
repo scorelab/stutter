f3fde3031c28c4bd9d3ac5ff91e249fb03405c97
rhperera <harith9127@gmail.com>
2015-05-24 13:49:51 +0530

    TabPagerAdapter added

---
+++ src/se/uu/daftest/TabPagerAdapter.java
@@ -0,0 +1,33 @@
+package se.uu.daftest;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+    public TabPagerAdapter(FragmentManager fm) {
+    super(fm);
+    // TODO Auto-generated constructor stub
+  }
+ 
+  @Override
+  public Fragment getItem(int i) {
+    switch (i) {
+        case 0:
+            //Fragement for Android Tab
+            return new HomeActivity();
+        case 1:
+           //Fragment for Ios Tab
+            return new MainActivityFragment();
+        }
+    return null;
+ 
+  }
+ 
+  @Override
+  public int getCount() {
+    // TODO Auto-generated method stub
+    return 2; //No of Tabs
+  }
+ 
+}

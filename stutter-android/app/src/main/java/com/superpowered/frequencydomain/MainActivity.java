5c53db0a507dad44e260e2009d7ed5f46067acb7
rhperera <harith9127@gmail.com>
2015-08-09 11:37:04 +0530

    Stutter Aid v2.0 with the support of Super powered Audio SDK

---
+++ stutter-android/app/src/main/java/com/superpowered/frequencydomain/MainActivity.java
@@ -0,0 +1,131 @@
+package com.superpowered.frequencydomain;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaRecorder;
+import android.os.*;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int buttonPressed=0;
+    int isFirst=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DAF daf = new DAF();
+        final Thread thread =  new Thread(daf);
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        final Button btn =(Button)findViewById(R.id.btn_start);
+        final Button testBtn = (Button)findViewById(R.id.btn_test_activity);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(buttonPressed==1){
+                    buttonPressed=0;
+                    btn.setText("start");
+                    StopRecord();
+                }
+                else {
+                    btn.setText("stop");
+                    buttonPressed = 1;
+                    AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+                    if (am.isBluetoothA2dpOn() || am.isWiredHeadsetOn()) {
+                        if (isFirst==0){
+                            StartRecord();
+                        }
+                        else{
+                            isFirst=0;
+                            thread.start();
+                        }
+
+                        System.out.println(thread.getPriority());
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Plug in headset",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,TestDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+    public void startRecording(){
+
+        // Get the device's sample rate and buffer size to enable low-latency Android audio output, if available.
+        String samplerateString = null, buffersizeString = null;
+        if (Build.VERSION.SDK_INT >= 17) {
+            AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+            samplerateString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+            buffersizeString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+        }
+        if(samplerateString==null) samplerateString= "44100";
+        if(buffersizeString==null) buffersizeString = "128";
+
+        System.loadLibrary("FrequencyDomain");
+        FrequencyDomain(Integer.parseInt(samplerateString), Integer.parseInt(buffersizeString));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private native void FrequencyDomain(long samplerate, long buffersize);
+    private native void StopRecord();
+    private native void StartRecord();
+
+    public class DAF implements Runnable{
+
+        public DAF(){
+           // android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+        }
+
+        @Override
+        public void run() {
+            startRecording();
+        }
+    }
+}
---
+++ stutter-android/app/src/main/obj/local/x86/objs/FrequencyDomain/FrequencyDomain.o.d
@@ -0,0 +1,88 @@
+I:/STUTTER/project/stutter/stutter-android/app/src/main/obj/local/x86/objs/FrequencyDomain/FrequencyDomain.o: \
+  I:/STUTTER/project/stutter/stutter-android/app/src/main/jni/FrequencyDomain.cpp \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\jni.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/cdefs.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/cdefs_elf.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\android/api-level.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\stdlib.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\string.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\malloc.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\alloca.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\strings.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/types.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\stdint.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/_types.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\machine/_types.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\linux/posix_types.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\linux/stddef.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\linux/compiler.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\asm/posix_types.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\asm/posix_types_32.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\asm/types.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\linux/types.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\machine/kernel.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/sysmacros.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\memory.h \
+  Superpowered\SuperpoweredFrequencyDomain.h \
+  Superpowered/SuperpoweredAudioBuffers.h \
+  Superpowered\SuperpoweredAndroidAudioIO.h \
+  Superpowered\SuperpoweredSimple.h Superpowered\SuperpoweredRecorder.h \
+  E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\stdio.h
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\jni.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/cdefs.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/cdefs_elf.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\android/api-level.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\stdlib.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\string.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\malloc.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\alloca.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\strings.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/types.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\stdint.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/_types.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\machine/_types.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\linux/posix_types.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\linux/stddef.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\linux/compiler.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\asm/posix_types.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\asm/posix_types_32.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\asm/types.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\linux/types.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\machine/kernel.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\sys/sysmacros.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\memory.h:
+
+Superpowered\SuperpoweredFrequencyDomain.h:
+
+Superpowered/SuperpoweredAudioBuffers.h:
+
+Superpowered\SuperpoweredAndroidAudioIO.h:
+
+Superpowered\SuperpoweredSimple.h:
+
+Superpowered\SuperpoweredRecorder.h:
+
+E:/Android/android-ndk/platforms/android-9/arch-x86/usr/include\stdio.h:

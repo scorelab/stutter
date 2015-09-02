8eca7fb5c155f6d1fd81605625d2be8af624f281
rhperera <harith9127@gmail.com>
2015-09-02 20:39:04 +0530

    web backend init

---
+++ stutter-web/application/views/footer.php
@@ -0,0 +1,10 @@
+<legend></legend>
+<div class="col-lg-8 col-lg-offset-2">
+  <footer style="text-align:center">
+    <p>
+      &copy; Sustainable Computing Research Group 2015
+    </p>
+  </footer>
+</div>
+</body>
+</html>
